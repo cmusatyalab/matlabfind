9378e5c77df0ba1fbc2e54d7b072ba020b6ed08f
Adam Goode <agoode@andrew.cmu.edu>
2007-02-06 19:43:05 +0000

    update for opendiamond release

--- src/quick_tar.h
+++ src/quick_tar.h
@@ -1,15 +1,15 @@
 /*
- * MATLABFind: A Diamond application for interoperating with MATLAB
+ *  MATLABFind
+ *  A Diamond application for interoperating with MATLAB
+ *  Version 1
  *
- * Copyright (c) 2006-2007 Carnegie Mellon University. All rights reserved.
- * Additional copyrights may be listed below.
+ *  Copyright (c) 2006-2007 Carnegie Mellon University
+ *  All Rights Reserved.
  *
- * This program and the accompanying materials are made available under
- * the terms of the Eclipse Public License v1.0 which accompanies this
- * distribution in the file named LICENSE.
- *
- * Technical and financial contributors are listed in the file named
- * CREDITS.
+ *  This software is distributed under the terms of the Eclipse Public
+ *  License, Version 1.0 which can be found in the file named LICENSE.
+ *  ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS SOFTWARE CONSTITUTES
+ *  RECIPIENT'S ACCEPTANCE OF THIS AGREEMENT
  */
 
 #ifndef __QUICK_TAR_H
