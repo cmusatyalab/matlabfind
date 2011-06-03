727d51b5b91b9c498ab90adff989f7a31ccb1a5e
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2011-06-02 20:47:13 -0400

    Store consistent timestamps in blob argument
    Every search was generating a different value for the blob argument,
    which defeated server-side result caching.

--- hyperfind-plugin/src/edu/cmu/cs/diamond/hyperfind/impl/MATLABSearch.java
+++ hyperfind-plugin/src/edu/cmu/cs/diamond/hyperfind/impl/MATLABSearch.java
@@ -98,7 +98,11 @@ public class MATLABSearch extends HyperFindSearch {
         ZipOutputStream zos = new ZipOutputStream(baos);
         try {
             for (Map.Entry<String, byte[]> entry: files.entrySet()) {
-                zos.putNextEntry(new ZipEntry(entry.getKey()));
+                ZipEntry ze = new ZipEntry(entry.getKey());
+                // storing different timestamps on every run would defeat
+                // server-side result caching
+                ze.setTime(0);
+                zos.putNextEntry(ze);
                 zos.write(entry.getValue(), 0, entry.getValue().length);
             }
             zos.close();
